4b334375c848f25385adec05df30eaa57a2c8087
dbrfgg <113384074+dbrfgg@users.noreply.github.com>
2023-05-30 23:18:29 +0300

    Update admin.php

--- back-6/admin.php
+++ back-6/admin.php
@@ -3,7 +3,7 @@
 if($_SERVER['REQUEST_METHOD']=='GET'){
   $user = 'u52940';
   $pass = '1327243';
-  $db = new PDO('mysql:host=localhost;dbname=u52927', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+  $db = new PDO('mysql:host=localhost;dbname=u52940', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
   $pass_hash=array();
   try{
     $get=$db->prepare("select pass from admin where user=?");
