f2ab9837b1f57ca60c3fa4265227924e7289e81a
dbrfgg <113384074+dbrfgg@users.noreply.github.com>
2023-05-30 23:19:40 +0300

    Update index.php

--- back-6/index.php
+++ back-6/index.php
@@ -93,7 +93,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   
   $user = 'u52940';
   $pass = '1327243';
-  $db = new PDO('mysql:host=localhost;dbname=u52927', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+  $db = new PDO('mysql:host=localhost;dbname=u52940', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
   try{
       $id=$_GET['edit_id'];
       $get=$db->prepare("SELECT * FROM form WHERE id=?");
@@ -205,7 +205,7 @@ else {
     
     $user = 'u52940';
     $pass = '1327243';
-    $db = new PDO('mysql:host=localhost;dbname=u52927', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+    $db = new PDO('mysql:host=localhost;dbname=u52940', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
     if(!$errors){
       $upd=$db->prepare("UPDATE form SET name=:name, email=:email, year=:byear, pol=:pol, limbs=:limbs, bio=:bio WHERE id=:id");
       $cols=array(
